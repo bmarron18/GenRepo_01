20040dfc56db8706bb57a6cca095fc37c85f3d94
bmarron18 <soil.foodwebs@gmail.com>
2025-10-17 13:45:02 -0600

    17Oct2025

--- Python/Python_TextManipulations/TextCleaning_UniqueWords_pdfWriter_OCR.py
+++ Python/Python_TextManipulations/TextCleaning_UniqueWords_pdfWriter_OCR.py
@@ -275,6 +275,54 @@ $ sudo mv Pillo* ~/.local/spyder-6/envs/spyder-runtime/lib/python3.11/site-packa
 
 
 # %%
+'''
+.txt to .pdf for translation purposes
+
+'''
+
+import textwrap
+from fpdf import FPDF
+from pathlib import Path
+import os
+
+
+   # set up
+input_filename = "/home/bmarron18/Desktop/test.txt"
+output_filename = "/home/bmarron18/Desktop/output.pdf"
+file = open(input_filename)
+text = file.read()
+file.close()
+
+
+def text_to_pdf(text, filename):
+    a4_width_mm = 210
+    pt_to_mm = 0.35
+    fontsize_pt = 10
+    fontsize_mm = fontsize_pt * pt_to_mm
+    margin_bottom_mm = 10
+    character_width_mm = 7 * pt_to_mm
+    width_text = a4_width_mm / character_width_mm
+
+    pdf = FPDF(orientation='P', unit='mm', format='A4')
+    pdf.set_auto_page_break(True, margin=margin_bottom_mm)
+    pdf.add_page()
+    pdf.set_font(family='Courier', size=fontsize_pt)
+    splitted = text.split('\n')
+
+    for line in splitted:
+        lines = textwrap.wrap(line, width_text)
+
+        if len(lines) == 0:
+            pdf.ln()
+
+        for wrap in lines:
+            pdf.cell(0, fontsize_mm, wrap, ln=1)
+
+    pdf.output(filename, 'F')
+
+
+    # run
+text_to_pdf(text, output_filename)
 
 
 
